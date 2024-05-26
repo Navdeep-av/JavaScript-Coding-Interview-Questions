d25ed771c76acc8d3ffa526cc9121e03bf7eb298
Navdeep-av <97311718+Navdeep-av@users.noreply.github.com>
2024-05-27 00:37:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var str = "Hello";
+var num = 5
+
+var start_loop = 1
+
+
+var new_str="";
+
+while(start_loop<=num){
+
+    var new_str = new_str +str+" ";
+    start_loop++
+}
+
+console.log(new_str)
