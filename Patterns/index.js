02e617ce073cfc6ac0d45e5b740dbf7b0de5c303
Navdeep-av <97311718+Navdeep-av@users.noreply.github.com>
2024-05-27 01:40:15 +0530

    Add files via upload

---
+++ Patterns/index.js
@@ -0,0 +1,20 @@
+for (i=5; i>0; i--){
+
+    var new_row = "";
+
+    for(j = 1; j<=i; j++){
+
+     var new_row = new_row+"*";   
+        
+}
+console.log(new_row)
+}
+
+// *****
+// ****
+// ***
+// **
+// *
+
+
+
---
+++ Patterns/index4.js
@@ -0,0 +1,23 @@
+// 11111
+// 2222
+// 333
+// 44
+// 5
+
+for (i=5; i>0; i--){
+
+    var new_row = "";
+
+    for(j = 1; j<i; j++){
+
+     var new_row = new_row+i;   
+        
+}
+console.log(new_row)
+}
+
+12345
+2345
+345
+45
+5
